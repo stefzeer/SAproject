ab221ca4f7217d59ef087a8a3c4304c897bdb05e
stefzeer <stefzeer90@gmail.com>
2020-03-10 11:46:31 +0200

    Create new1-stackexchange2.py

---
+++ new1-stackexchange2.py
@@ -0,0 +1,19 @@
+import paramiko
+import os
+
+ssh = paramiko.SSHClient()
+
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+command=input("Enter a command to run on the remote server(s): ")
+user_input=input("Enter the path of your file: ")
+if os.path.exists(user_input):
+
+ with open(user_input, 'r') as fp:
+    for ip in [line.strip() for line in fp.readlines()]:
+        ssh.connect(ip, 22, "root", "Stefan9010")
+        stdin, stdout, stderr = ssh.exec_command(command)
+        print (ip, stdout.readline())
+        ssh.close()
+
+else:
+    print("The file does not exist")
